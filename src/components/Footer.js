904125af6b1f3f3d71e4dc74d9eda966a2f30aa1
vijaymahes9080 <vijaymahes9080@gmail.com>
2026-05-26 12:09:56 +0530

    Update footer link to own GitHub Pages URL

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Img from '../assets/img/github.png';
 const Footer = () => {
   return (
     <>
-      <a href="https://github.com/atanu20/dreamhouse" target="_blank">
+      <a href="https://vijaymahes9080.github.io/dreamhouse_react/" target="_blank" rel="noreferrer">
         <div class="fixed w-12 h-12 animate-bounce flex items-center justify-center bg-white rounded-full bottom-0 right-2">
           <img src={Img} alt="" />
         </div>
