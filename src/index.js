4316021ef938f93fc1fa7fa492dfd7c66c841c9e
vijaymahes9080 <vijaymahes9080@gmail.com>
2026-05-26 12:00:22 +0530

    Configure GitHub Pages deployment with gh-pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HouseContextProvider>
-    <Router>
+    <Router basename="/dreamhouse_react">
       <React.StrictMode>
         <App />
       </React.StrictMode>
